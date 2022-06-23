a66e9175d2843cd9c4b463735415a4b2caf1d1b9
alexlee94 <alexlee94@protonmail.com>
2022-06-22 20:10:25 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+	return (
+		<header className="header">
+			<img src="./images/troll-face.png" className="header--image" />
+			<h2 className="header--title">Meme Generator</h2>
+			<h4 className="header--project">
+				<a
+					href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+					className="header--project--link"
+				>
+					Buy me a coffee! ☕
+				</a>
+			</h4>
+		</header>
+	);
+}
